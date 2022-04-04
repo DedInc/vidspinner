184b1a8dbbc229d70ea6d8034b727d03847d59a3
DedInc <visitanimation@gmail.com>
2022-04-04 11:47:43 +0600

    v1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='vidspinner',
-    version='1.0.0',
+    version='1.0.1',
     author='Maehdakvan',
     author_email='visitanimation@google.com',
     description='Video uniqualizer.',
